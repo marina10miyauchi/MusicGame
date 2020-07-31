e4905099c50c0a4d3d958ac67113998485068315
Marina_Miyauchi <mari01miya@gmail.com>
2020-07-31 10:45:45 +0900

    ややこいし邪魔だしエラー出るしめんどくさいから削除

--- Game/Game/MusicNotes.cpp
+++
@@ -1,9 +0,0 @@
-#include "MusicNotes.h"
-
-MusicNotes::MusicNotes(IWorld & _world, NotesState _state, Vector2 _position, float _timing, int lane)
-{
-}
-
-void MusicNotes::Updata(float _deltaTime)
-{
-}
